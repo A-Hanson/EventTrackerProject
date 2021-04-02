a504167c6e31f71dc9f5681621ca489c06b75286
Anna Hanson <>
2021-04-02 15:36:10 -0600

    Full-stack smoke test

--- TVTrackerApp/src/main/java/com/skilldistillery/jpatvtracker/controllers/TvWatchingSessionController.java
+++ TVTrackerApp/src/main/java/com/skilldistillery/jpatvtracker/controllers/TvWatchingSessionController.java
@@ -1,10 +1,13 @@
 package com.skilldistillery.jpatvtracker.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.skilldistillery.jpatvtracker.entities.TvWatchingSession;
 import com.skilldistillery.jpatvtracker.services.TvWatchingSessionService;
 
 @RestController
@@ -12,11 +15,16 @@ import com.skilldistillery.jpatvtracker.services.TvWatchingSessionService;
 public class TvWatchingSessionController {
 
 	@Autowired
-	private TvWatchingSessionService service;
+	private TvWatchingSessionService svc;
 	
 	@GetMapping("ping")
 	public String test() {
 		return "pong";
+	}
+	
+	@GetMapping("tv_watching_sessions")
+	public List<TvWatchingSession> listSessions(){
+		return svc.allTvWatchingSessions();
 	}
 	
 }
