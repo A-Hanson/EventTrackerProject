b8c23ff6d7a0baaf1e756af0494453f273f49ec5
Anna Hanson <>
2021-04-02 15:05:38 -0600

    link Spring Boot project to JPA project

---
+++ TVTrackerApp/src/main/java/com/skilldistillery/jpatvtracker/TvTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpatvtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvTrackerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvTrackerAppApplication.class, args);
+	}
+
+}
---
+++ TVTrackerApp/src/test/java/com/skilldistillery/jpatvtracker/TvTrackerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpatvtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvTrackerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
