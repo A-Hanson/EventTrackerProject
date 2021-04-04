18d8959321339f861d2cdd21b33971de698486c9
Anna Hanson <>
2021-04-04 08:43:00 -0600

    finish basic CRUD controller api mapping

--- TVTrackerApp/src/main/java/com/skilldistillery/jpatvtracker/controllers/TvWatchingSessionController.java
+++ TVTrackerApp/src/main/java/com/skilldistillery/jpatvtracker/controllers/TvWatchingSessionController.java
@@ -6,9 +6,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -62,6 +64,45 @@ public class TvWatchingSessionController {
 		return tvWatchingSession;
 	}
 	
-//	TODO: FINISH MAPPING
+	@PutMapping("tv_watching_sessions/{id}")
+	public TvWatchingSession updateSession(@PathVariable int id,
+			@RequestBody TvWatchingSession tvWatchingSession,
+			HttpServletResponse response,
+			HttpServletRequest request) {
+		try {
+			tvWatchingSession = svc.updateSession(id, tvWatchingSession);
+			if (tvWatchingSession == null) {
+				response.setStatus(404);
+			}
+			response.setStatus(200);
+			StringBuffer url = request.getRequestURL();			
+			url.append("/").append(tvWatchingSession.getId());
+			response.setHeader("location", url.toString());	
+			
+		}
+		catch (Exception e){
+			System.err.println(e);
+			response.setStatus(400);
+			tvWatchingSession = null;
+		}
+		return tvWatchingSession;
+	}
+	
+	@DeleteMapping("tv_watching_sessions/{id}")
+	public void updateSession(@PathVariable int id,
+			HttpServletResponse response) {
+		try {
+			boolean deleted = svc.softDeleteSession(id);
+			if (deleted) {
+				response.setStatus(204);
+			} else {
+				response.setStatus(404);
+			}
+		} catch (Exception e) {
+			System.err.println(e);
+			response.setStatus(400);
+		}
+	}
+
 	
 }
