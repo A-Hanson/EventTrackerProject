631446f0081359c5549e433f72a40bea1cee8656
Anna Hanson <>
2021-04-10 17:28:37 -0600

    add session form completed, JavaScript object completed

--- TVTrackerApp/src/main/resources/static/js/script.js
+++ TVTrackerApp/src/main/resources/static/js/script.js
@@ -16,12 +16,55 @@ function init() {
 }
 
 function createTVSession(){
-// TODO
+  let session = {};
+  session['start'] = document.addSession.start.value;
+  session['stop'] = document.addSession.stop.value;
+  session['platform'] = {id: Number(document.addSession.platform.value)};
+  session['user'] = {id: Number(document.addSession.user.value)};
+  sendTVSessionToApi(session);
+  addSession.reset();
+}
+
+function sendTVSessionToApi(session){
+
 }
 
 function displayTVSession(session){
 // TODO
 }
+
+// function loadUserById(id){
+//   let xhr = new XMLHttpRequest();
+//   xhr.open('GET', 'api/users/' + id);
+//   xhr.onreadystatechange = function () {
+//     if (xhr.readyState === 4){
+//       if (xhr.status === 200){
+//         let user = JSON.parse(xhr.responseText);
+//         return user;
+//       } else {
+//         displayError("Error retrieving user: " + xhr.status);
+//       }
+//     }
+//   }
+//   xhr.send();  
+// }
+
+// function loadPlatformById(id){
+//   let xhr = new XMLHttpRequest();
+//   xhr.open('GET', 'api/platforms/' + id);
+//   xhr.onreadystatechange = function () {
+//     if (xhr.readyState === 4){
+//       if (xhr.status === 200){
+//         let platform = JSON.parse(xhr.responseText);
+//         console.log(platform);
+//         return platform;
+//       } else {
+//         displayError("Error retrieving platform: " + xhr.status);
+//       }
+//     }
+//   }
+//   xhr.send();  
+// }
 
 function loadTVSessions() {
   let xhr = new XMLHttpRequest();
