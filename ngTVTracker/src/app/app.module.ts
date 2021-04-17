bbbc3524c1c48e971a8a335daa214dd1d00e6e3f
Anna Hanson <>
2021-04-16 18:29:35 -0600

    move files under misspelled folder names, add base level files

--- ngTVTracker/src/app/app.module.ts
+++ ngTVTracker/src/app/app.module.ts
@@ -5,12 +5,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TvwatchingsessionService } from './services/tvwatchingsession.service';
-import { TvWatchingSessionListComponent } from './componenets/tv-watching-session-list/tv-watching-session-list.component';
+import { TvWatchingSessionListComponent } from './components/tv-watching-session-list/tv-watching-session-list.component';
+import { PlatformListComponent } from './components/platform-list/platform-list.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TvWatchingSessionListComponent
+    TvWatchingSessionListComponent,
+    PlatformListComponent,
+    NotfoundComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
